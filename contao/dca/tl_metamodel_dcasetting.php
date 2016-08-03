99a6cf5c7087118c661d7057a0033b50f2d52960
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-08-04 00:19:59 +0200

    Fix code style issues

--- contao/dca/tl_metamodel_dcasetting.php
+++ contao/dca/tl_metamodel_dcasetting.php
@@ -1,29 +1,31 @@
 <?php
 
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_url.
  *
- * PHP version 5
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels
  * @subpackage AttributeUrl
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     Christopher Boelter <christopher@boelter.eu>
- * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_url/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
 $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['url'] = array
 (
-	'presentation' => array(
-		'tl_class',
-	),
-	'functions'  => array(
-		'mandatory',
-	)
+    'presentation' => array(
+        'tl_class',
+    ),
+    'functions'  => array(
+        'mandatory',
+    )
 );
